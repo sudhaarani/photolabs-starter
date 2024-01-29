45aaa36cb30bbb4ee8bfeccfaeb2241cdf38176b
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-01-29 13:45:02 -0800

    display photo details from props

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,15 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
   /* Insert React */
+  return (<>
+    <img src={props.sampleDataForPhotoListItem.imageSource} />
+    <img src={props.sampleDataForPhotoListItem.profile} />
+    <p>{props.sampleDataForPhotoListItem.username}</p>
+    <p>{props.sampleDataForPhotoListItem.location.city}, {props.sampleDataForPhotoListItem.location.country}</p>
+    </>
+ )
 };
 
 export default PhotoListItem;
