04cf3bfbe63d9054f2bca1f655b7f8ef0eb71667
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-01-31 11:00:39 -0800

    use id props when rendering components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,9 @@ const sampleDataForPhotoListItem = {
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  const photos = new Array(3).fill(<PhotoListItem location={sampleDataForPhotoListItem.location}
+  const photos = new Array(3).fill(
+    <PhotoListItem key={sampleDataForPhotoListItem.id}
+      location={sampleDataForPhotoListItem.location}
         imageSource={sampleDataForPhotoListItem.imageSource}
         username={sampleDataForPhotoListItem.username}
         profile={sampleDataForPhotoListItem.profile}
