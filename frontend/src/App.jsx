6bbafbdcc94fa189d2c2f878169c9c69ba7455f9
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-01-31 13:16:19 -0800

    Remove sampleDataForPhotoListItem and Array of PhotoListItem ,render PhotoList component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,22 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
+//import PhotoListItem from './components/PhotoListItem';
+import PhotoList from './components/PhotoList';
 import './App.scss';
-
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  const photos = new Array(3).fill(
-    <PhotoListItem key={sampleDataForPhotoListItem.id}
-      location={sampleDataForPhotoListItem.location}
-        imageSource={sampleDataForPhotoListItem.imageSource}
-        username={sampleDataForPhotoListItem.username}
-        profile={sampleDataForPhotoListItem.profile}
-      />)
+  // const photos = new Array(3).fill(
+  //   <PhotoListItem key={sampleDataForPhotoListItem.id}
+  //     location={sampleDataForPhotoListItem.location}
+  //       imageSource={sampleDataForPhotoListItem.imageSource}
+  //       username={sampleDataForPhotoListItem.username}
+  //       profile={sampleDataForPhotoListItem.profile}
+  //     />)
 
   return (
     <div className="App">
-      {photos}
+      <PhotoList />
     </div>
   );
 };
