e3166550115b1d56053317be99435956230c2ffd
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-01-31 12:27:39 -0800

    add a favourite icon to a Photo and add a click listener to the icon using the onClick attribute to like and unlike photos

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,16 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [like, setLike] = useState(false);
+  const handleClick = () => {
+    setLike(prev => !prev)
+  }
+
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={handleClick}>
       <div className="photo-list__fav-icon-svg">
         {/* Insert React */}
+        <FavIcon selected={like} />
       </div>
     </div>
   );
