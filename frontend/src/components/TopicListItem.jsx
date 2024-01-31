79691732c987b1cb5069160d2ab28312f1476056
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-01-31 15:28:24 -0800

    Remove sampleDataForTopicListItem and display title

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -2,16 +2,17 @@ import React from "react";
 
 import "../styles/TopicListItem.scss";
 
-const sampleDataForTopicListItem = {
-  id: "1",
-  slug: "topic-1",
-  label: "Nature",
-};
+// const sampleDataForTopicListItem = {
+//   id: "1",
+//   slug: "topic-1",
+//   label: "Nature",
+// };
 
-const TopicListItem = () => {
+const TopicListItem = ({key,title}) => {
   return (
     <div className="topic-list__item">
       {/* Insert React */}
+      <p>{title}</p>
     </div>
   );
 };
