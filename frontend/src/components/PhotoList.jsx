edfb48667f6ce271a94e9dfbb701b8de89245ff3
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-02-01 00:00:12 -0800

    add mock photos

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -2,69 +2,71 @@ import React from "react";
 
 import "../styles/PhotoList.scss";
 import PhotoListItem from './PhotoListItem';
+import photos from "../mocks/photos";
 
-const sampleDataForPhotoList = [
-  {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    },
-    user: {
-      id: "1",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-  {
-    id: "2",
-    location: {
-      city: "Toronto",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
-    },
-    user: {
-      id: "2",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-  {
-    id: "3",
-    location: {
-      city: "Ottawa",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
-    },
-    user: {
-      id: "3",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-];
+// const sampleDataForPhotoList = [
+//   {
+//     id: "1",
+//     location: {
+//       city: "Montreal",
+//       country: "Canada",
+//     },
+//     urls: {
+//       full: `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
+//       regular: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+//     },
+//     user: {
+//       id: "1",
+//       username: "exampleuser",
+//       name: "Joe Example",
+//       profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+//     },
+//   },
+//   {
+//     id: "2",
+//     location: {
+//       city: "Toronto",
+//       country: "Canada",
+//     },
+//     urls: {
+//       full: `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
+//       regular: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
+//     },
+//     user: {
+//       id: "2",
+//       username: "exampleuser",
+//       name: "Joe Example",
+//       profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+//     },
+//   },
+//   {
+//     id: "3",
+//     location: {
+//       city: "Ottawa",
+//       country: "Canada",
+//     },
+//     urls: {
+//       full: `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
+//       regular: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
+//     },
+//     user: {
+//       id: "3",
+//       username: "exampleuser",
+//       name: "Joe Example",
+//       profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+//     },
+//   },
+// ];
 
 const PhotoList = () => {
-  const photoListArray = sampleDataForPhotoList.map((PhotoLists) =>{
-   return (<PhotoListItem key={PhotoLists.id}
+  const photoListArray = photos.map((PhotoLists) => {
+    return (<PhotoListItem key={PhotoLists.id}
       location={PhotoLists.location}
-        imageSource={PhotoLists.urls.regular}
-        username={PhotoLists.user.name}
-        profile={PhotoLists.user.profile}
-      />)}
+      imageSource={PhotoLists.urls.regular}
+      username={PhotoLists.user.name}
+      profile={PhotoLists.user.profile}
+    />)
+  }
   );
 
   return (
