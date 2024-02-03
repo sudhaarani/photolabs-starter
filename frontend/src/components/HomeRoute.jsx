06350db49f873cd4178904626183e35533eed96a
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-02-03 00:57:13 -0800

    OnClick event listenser function is sent as props to PhotoList

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -4,13 +4,13 @@ import "../styles/HomeRoute.scss";
 import PhotoList from './PhotoList';
 import TopNavigation from "./TopNavigationBar"
 
-const HomeRoute = ({ photos, topics, favPhotos, favPhotosClick, isFavPhotoExist }) => {
+const HomeRoute = ({ photos, topics, favPhotos, favPhotosClick, isFavPhotoExist, showModal }) => {
   //console.log("home route: topics from props::", topics);
 
   return (
     <div className="home-route">
       <TopNavigation topics={topics} favPhotos={favPhotos} isFavPhotoExist={isFavPhotoExist} />
-      <PhotoList photos={photos} favPhotos={favPhotos} favPhotosClick={favPhotosClick} />
+      <PhotoList photos={photos} favPhotos={favPhotos} favPhotosClick={favPhotosClick} showModal={showModal} />
     </div>
   );
 }
