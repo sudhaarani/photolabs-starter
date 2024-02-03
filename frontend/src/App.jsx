b2b7af0e97836d7849087d8199fef3e9e4167a7e
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-02-02 23:42:09 -0800

    lifting up the state change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,16 @@ import topics from "./mocks/topics";
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   const [favPhotos, setFavPhotos] = useState([]);
+  const favorite = (id) => {
+    setFavPhotos(prev => {
+
+      console.log("prev:", prev)
+      return prev.includes(id) ? prev.filter(_id =>
+        _id !== id
+      ) : [...prev, id]
+    })
+  }
+
   // const photos = new Array(3).fill(
   //   <PhotoListItem key={sampleDataForPhotoListItem.id}
   //     location={sampleDataForPhotoListItem.location}
@@ -19,7 +29,8 @@ const App = () => {
   console.log("app: topics from array", topics);
   return (
     <div className="App">
-      <HomeRoute photos={photos} topics={topics} favPhotos={favPhotos} setFavPhotos={setFavPhotos} />
+      <HomeRoute photos={photos} topics={topics} favPhotos={favPhotos} favPhotosClick={favorite}
+        isFavPhotoExist={favPhotos.length > 0 ? true : false} />
     </div>
   );
 };
