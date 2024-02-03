4ec7e893a1611524aa2376f81900cadbea125e6d
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-02-02 23:43:05 -0800

    lifting up the state change

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import PhotoFavButton from "./PhotoFavButton";
 import "../styles/PhotoListItem.scss";
 
-const PhotoListItem = ({ id, location, imageSource, profile, username, favPhotos, setFavPhotos }) => { //instead of props
+const PhotoListItem = ({ id, location, imageSource, profile, username, favPhotos, favPhotosClick }) => { //instead of props
   /* Insert React */
   // console.log(props.key)
   console.log("PhotoListItem")
   return (<div className="photo-list__item">
-    <PhotoFavButton id={id} favPhotos={favPhotos} setFavPhotos={setFavPhotos} />
+    <PhotoFavButton id={id} favPhotos={favPhotos} favPhotosClick={favPhotosClick} />
     <img className="photo-list__image" src={imageSource} />
     <div className="photo-list__user-details">
       <img className="photo-list__user-profile" src={profile} />
