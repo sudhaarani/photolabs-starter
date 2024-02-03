0f3bdd8d6bb52ebe168af405600eaeb33a15f5c6
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-02-03 15:38:50 -0800

    props destructure to get setDisplayModal and send to PhotoListItem component

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -4,13 +4,16 @@ import "../styles/HomeRoute.scss";
 import PhotoList from './PhotoList';
 import TopNavigation from "./TopNavigationBar"
 
-const HomeRoute = ({ photos, topics, favPhotos, favPhotosClick, isFavPhotoExist, showModal }) => {
+const HomeRoute = ({ photos, topics, favPhotos, favPhotosClick, isFavPhotoExist, setDisplayModal,
+  handleModalVisibility }) => {
   //console.log("home route: topics from props::", topics);
 
   return (
     <div className="home-route">
       <TopNavigation topics={topics} favPhotos={favPhotos} isFavPhotoExist={isFavPhotoExist} />
-      <PhotoList photos={photos} favPhotos={favPhotos} favPhotosClick={favPhotosClick} showModal={showModal} />
+      <PhotoList photos={photos} favPhotos={favPhotos} favPhotosClick={favPhotosClick}
+        setDisplayModal={setDisplayModal}
+        handleModalVisibility={handleModalVisibility} />
     </div>
   );
 }
