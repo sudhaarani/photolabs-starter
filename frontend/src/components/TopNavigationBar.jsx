137f3866b6d633435266205006a97ff7a750a502
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-02-02 23:43:30 -0800

    lifting up the state change

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -3,14 +3,14 @@ import TopicList from './TopicList';
 import FavBadge from "./FavBadge";
 import '../styles/TopNavigationBar.scss';
 
-const TopNavigation = ({ topics, favPhotos }) => {
+const TopNavigation = ({ topics, favPhotos, isFavPhotoExist }) => {
 
   // console.log("TopNavigation: topics from props", topics);
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList topics={topics} />
-      <FavBadge isFavPhotoExist={favPhotos.length > 0 ? true : false} />
+      <FavBadge isFavPhotoExist={isFavPhotoExist} />
     </div>
   )
 }
