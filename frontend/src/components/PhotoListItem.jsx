9503f6e3f5b378eaf358de16f23c99a39ec328a3
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-02-03 15:41:56 -0800

    props destructure to get setDisplayModal and set it to true when img is clicked

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import PhotoFavButton from "./PhotoFavButton";
 import "../styles/PhotoListItem.scss";
 
-const PhotoListItem = ({ id, location, imageSource, profile, username, favPhotos, favPhotosClick, showModal }) => { //instead of props
+const PhotoListItem = ({ id, location, imageSource, profile, username, favPhotos, favPhotosClick,
+  handleModalVisibility, setDisplayModal }) => { //instead of props
   /* Insert React */
   // console.log(props.key)
-  console.log("PhotoListItem")
+  console.log("PhotoListItem::", id)
   return (<div className="photo-list__item">
     <PhotoFavButton id={id} favPhotos={favPhotos} favPhotosClick={favPhotosClick} />
-    <img className="photo-list__image" src={imageSource} onClick={() => { showModal() }} />
+    <img className="photo-list__image" src={imageSource} onClick={() => {
+      setDisplayModal(true)
+      handleModalVisibility(id)
+    }} />
     <div className="photo-list__user-details">
       <img className="photo-list__user-profile" src={profile} />
       <div>
