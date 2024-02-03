c6640b4919065be6dcc3193969c3f11867503a8c
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-02-02 23:42:37 -0800

    lifting up the state change

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,22 +3,19 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton({ setFavPhotos, id }) {
+function PhotoFavButton({ favPhotosClick, id }) {
   const [like, setLike] = useState(false);
   const handleClick = () => {
     setLike(prev => !prev)
     // if like is true ,add to array
-    setFavPhotos(prev => {
-      console.log("like:", like)
-      console.log("prev:", prev)
-      return !like ? [...prev, id] : prev.filter(_id =>
-        _id !== id
-      )
-    })
+
   }
 
   return (
-    <div className="photo-list__fav-icon" onClick={handleClick}>
+    <div className="photo-list__fav-icon" onClick={() => {
+      handleClick()
+      favPhotosClick(id)
+    }}>
       <div className="photo-list__fav-icon-svg">
         {/* Insert React */}
         <FavIcon selected={like} />
