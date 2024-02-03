418d9df164b1337318bf738af07a9be3bad22f8f
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-02-02 22:44:24 -0800

    assign global state to track the favourited photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //import PhotoListItem from './components/PhotoListItem';
 import HomeRoute from './components/HomeRoute';
 import './App.scss';
+import photos from "./mocks/photos";
+import topics from "./mocks/topics";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const [favPhotos, setFavPhotos] = useState([]);
   // const photos = new Array(3).fill(
   //   <PhotoListItem key={sampleDataForPhotoListItem.id}
   //     location={sampleDataForPhotoListItem.location}
@@ -13,10 +16,10 @@ const App = () => {
   //       username={sampleDataForPhotoListItem.username}
   //       profile={sampleDataForPhotoListItem.profile}
   //     />)
-
+  console.log("app: topics from array", topics);
   return (
     <div className="App">
-      <HomeRoute/>
+      <HomeRoute photos={photos} topics={topics} favPhotos={favPhotos} setFavPhotos={setFavPhotos} />
     </div>
   );
 };
