75b0402510da6226b4dff94a829c3f0ba0cd02b1
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-02-02 22:42:06 -0800

    Eslint

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -2,13 +2,7 @@ import React from "react";
 
 import "../styles/TopicListItem.scss";
 
-// const sampleDataForTopicListItem = {
-//   id: "1",
-//   slug: "topic-1",
-//   label: "Nature",
-// };
-
-const TopicListItem = ({ key, title }) => {
+const TopicListItem = ({ title }) => {
   return (
     <div className="topic-list__item">
       {/* Insert React */}
