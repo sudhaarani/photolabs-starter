8cce5ca87022bcd4a0f3f38d19cf2c701f719d56
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-02-02 22:41:58 -0800

    Eslint

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,33 +2,15 @@ import React from "react";
 
 import "../styles/TopicList.scss";
 import TopicListItem from './TopicListItem';
-import topics from "../mocks/topics";
 
-// const sampleDataForTopicList = [
-//   {
-//     id: "1",
-//     slug: "topic-1",
-//     title: "Nature",
-//   },
-//   {
-//     id: "2",
-//     slug: "topic-2",
-//     title: "Travel",
-//   },
-//   {
-//     id: "3",
-//     slug: "topic-3",
-//     title: "People",
-//   },
-// ];
-
-const TopicList = () => {
-  const topicListArray = topics.map((topicLists) => {
+const TopicList = (props) => {
+  // console.log(props)
+  // console.log("props.topics:", props.topics)
+  const topicListArray = props.topics.map((topicLists) => {
     return (<TopicListItem key={topicLists.id}
       title={topicLists.title}
     />)
-  }
-  );
+  });
 
   return (
     <div className="top-nav-bar__topic-list">
