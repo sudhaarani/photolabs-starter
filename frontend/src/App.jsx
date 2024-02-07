cb23d0517b050a1cd704361f4c800872c46a42db
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-02-07 01:07:24 -0800

    import required variables and functions from useApplicationData custom hook and send as props to components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,30 +3,22 @@ import React from 'react';
 import HomeRoute from './components/HomeRoute';
 import PhotoDetailsModal from './routes/PhotoDetailsModal';
 import './App.scss';
-import photos from "./mocks/photos";
-import topics from "./mocks/topics";
 import useApplicationData from "./hooks/useApplicationData";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   const {
-    state, setDisplayModal,
+    state, setDisplayModal, getTopicId,
     favorite, handleModalPhoto
   } = useApplicationData();
 
-  // const photos = new Array(3).fill(
-  //   <PhotoListItem key={sampleDataForPhotoListItem.id}
-  //     location={sampleDataForPhotoListItem.location}
-  //       imageSource={sampleDataForPhotoListItem.imageSource}
-  //       username={sampleDataForPhotoListItem.username}
-  //       profile={sampleDataForPhotoListItem.profile}
-  //     />)
-  console.log("app: topics from array", topics);
+  console.log("app: topics from array", state.topics);
+  console.log("app: photos from array", state.photos);
 
   return (
     <div className="App">
-      <HomeRoute photos={photos} topics={topics} favPhotos={state.favPhotos} favPhotosClick={favorite}
-        isFavPhotoExist={state.favPhotos.length > 0 ? true : false}
+      <HomeRoute photos={state.photos} topics={state.topics} favPhotos={state.favPhotos} favPhotosClick={favorite}
+        isFavPhotoExist={state.favPhotos.length > 0 ? true : false} getTopicId={getTopicId}
         handleModalVisibility={handleModalPhoto} setDisplayModal={setDisplayModal} />
       {state.displayModal && <PhotoDetailsModal setDisplayModal={setDisplayModal} selectedPhoto={state.selectedPhoto}
         favPhotos={state.favPhotos} favPhotosClick={favorite} />}
