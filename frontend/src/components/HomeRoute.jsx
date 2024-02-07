640ff1a4106af429191a3c4f26bdaee1788f5f12
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-02-07 00:59:29 -0800

    import required variables and functions from useApplicationData custom hook and send as props to components

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -4,12 +4,12 @@ import "../styles/HomeRoute.scss";
 import PhotoList from './PhotoList';
 import TopNavigation from "./TopNavigationBar"
 
-const HomeRoute = ({ photos, topics, favPhotos, favPhotosClick, isFavPhotoExist, setDisplayModal,
+const HomeRoute = ({ photos, topics, favPhotos, favPhotosClick, isFavPhotoExist, setDisplayModal, getTopicId,
   handleModalVisibility }) => {
 
   return (
     <div className="home-route">
-      <TopNavigation topics={topics} favPhotos={favPhotos} isFavPhotoExist={isFavPhotoExist} />
+      <TopNavigation topics={topics} favPhotos={favPhotos} isFavPhotoExist={isFavPhotoExist} getTopicId={getTopicId} />
       <PhotoList photos={photos} favPhotos={favPhotos} favPhotosClick={favPhotosClick}
         setDisplayModal={setDisplayModal}
         handleModalVisibility={handleModalVisibility} />
