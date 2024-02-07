8ffcdd0c353f40685d5dc1d8ad0d98486714fdbb
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-02-07 01:06:29 -0800

    remove comments

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,8 +5,6 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = ({ id, location, imageSource, profile, username, favPhotos, favPhotosClick,
   handleModalVisibility, setDisplayModal }) => { //instead of props
   /* Insert React */
-  // console.log(props.key)
-  //console.log("PhotoListItem::", id)
   return (<div className="photo-list__item">
     <PhotoFavButton id={id} favPhotos={favPhotos} favPhotosClick={favPhotosClick} />
     <img className="photo-list__image" src={imageSource} onClick={() => {
