89cf3d9c5f2687e9dfe15b54c2732253fe78a78b
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-02-08 13:01:35 -0800

    destructure the props

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -3,14 +3,13 @@ import TopicList from './TopicList';
 import FavBadge from "./FavBadge";
 import '../styles/TopNavigationBar.scss';
 
-const TopNavigation = ({ topics, isFavPhotoExist, getTopicId }) => {
+const TopNavigation = ({ state, isFavPhotoExist, getTopicId }) => {
 
-  // console.log("TopNavigation: topics from props", topics);
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList topics={topics} getTopicId={getTopicId} />
-      <FavBadge isFavPhotoExist={isFavPhotoExist} />  
+      <TopicList topics={state.topics} getTopicId={getTopicId} />
+      <FavBadge isFavPhotoExist={isFavPhotoExist} />
     </div>
   )
 }
