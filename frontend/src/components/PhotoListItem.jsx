1520a8c3ac34e82cb459d9ca8488f44e21d4d713
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-02-08 13:00:34 -0800

    destructure the photoList object

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import PhotoFavButton from "./PhotoFavButton";
 import "../styles/PhotoListItem.scss";
 
-const PhotoListItem = ({ id, location, imageSource, profile, username, favPhotos, favPhotosClick,
+const PhotoListItem = ({ photoList, state, favPhotosClick,
   handleModalVisibility, setDisplayModal }) => { //instead of props
   /* Insert React */
+  const { id, location, urls, user } = photoList;
+
   return (<div className="photo-list__item">
-    <PhotoFavButton id={id} favPhotos={favPhotos} favPhotosClick={favPhotosClick} />
-    <img className="photo-list__image" src={imageSource} onClick={() => {
+    <PhotoFavButton id={id} favPhotos={state.favPhotos} favPhotosClick={favPhotosClick} />
+    <img className={state.displayModal ? "photo-list__modal_image" : "photo-list__image"} src={urls.regular} onClick={() => {
       setDisplayModal()
       handleModalVisibility(id)
     }} />
     <div className="photo-list__user-details">
-      <img className="photo-list__user-profile" src={profile} />
+      <img className="photo-list__user-profile" src={user.profile} />
       <div>
-        <p className="photo-list__user-info">{username}</p>
+        <p className="photo-list__user-info">{user.name}</p>
         <p className="photo-list__user-location">{location.city}, {location.country}</p>
       </div>
     </div>
