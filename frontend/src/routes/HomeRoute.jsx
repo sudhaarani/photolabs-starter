6cd70825f0188f4b2b638fbb6535947127cc7420
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-02-08 12:44:04 -0800

    add this file from another folder and send state as props to child components

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
 import '../styles/HomeRoute.scss';
+import PhotoList from '../components/PhotoList';
+import TopNavigation from "../components/TopNavigationBar"
 
-const HomeRoute = () => {
+const HomeRoute = ({ state, favPhotosClick, isFavPhotoExist, setDisplayModal, getTopicId,
+  handleModalVisibility }) => {
+
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigation state={state} isFavPhotoExist={isFavPhotoExist} getTopicId={getTopicId} />
+      <PhotoList state={state} favPhotosClick={favPhotosClick}
+        setDisplayModal={setDisplayModal}
+        handleModalVisibility={handleModalVisibility} />
     </div>
   );
-};
+}
 
 export default HomeRoute;
