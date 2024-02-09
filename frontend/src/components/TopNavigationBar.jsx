f9900dd36c1d0d9b59d0cae0881a4f5fe82d04eb
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-02-08 19:33:23 -0800

    Create loading skeleton states when loading navigation bar

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import TopicList from './TopicList';
 import FavBadge from "./FavBadge";
 import '../styles/TopNavigationBar.scss';
+import Skeleton from '@mui/material/Skeleton';
 
 const TopNavigation = ({ state, isFavPhotoExist, getTopicId }) => {
 
   return (
     <div className="top-nav-bar">
-      <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList topics={state.topics} getTopicId={getTopicId} />
-      <FavBadge isFavPhotoExist={isFavPhotoExist} />
+      {state.loading ? (
+        <Skeleton variant="rectangular" width="1500px" height="72px" />
+      ) : (
+        <>
+          <span className="top-nav-bar__logo">PhotoLabs</span>
+          <TopicList state={state} getTopicId={getTopicId} />
+          <FavBadge isFavPhotoExist={isFavPhotoExist} />
+
+        </>
+      )}
     </div>
   )
 }
