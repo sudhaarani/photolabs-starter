75fd2eb02d6b74c4055da997ee314f32aab5bea9
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-02-08 19:33:57 -0800

    destructure state from props

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -3,10 +3,10 @@ import React from "react";
 import "../styles/TopicList.scss";
 import TopicListItem from './TopicListItem';
 
-const TopicList = (props) => {
-  const topicListArray = props.topics.map((topicList) => {
+const TopicList = ({ state, getTopicId }) => {
+  const topicListArray = state.topics.map((topicList) => {
     return (<TopicListItem key={topicList.id}
-      title={topicList.title} id={topicList.id} getTopicId={props.getTopicId}
+      title={topicList.title} id={topicList.id} getTopicId={getTopicId}
     />)
   });
 
