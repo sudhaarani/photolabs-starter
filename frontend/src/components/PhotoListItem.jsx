13c659814700d69b3238612bc0faf1caaddce603
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2024-02-08 19:22:01 -0800

    Create loading skeleton states when loadingphotos

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import PhotoFavButton from "./PhotoFavButton";
 import "../styles/PhotoListItem.scss";
 import Skeleton from '@mui/material/Skeleton';
-import Typography, { TypographyProps } from '@mui/material/Typography';
+
 const PhotoListItem = ({ photoList, state, favPhotosClick,
   handleModalVisibility, setDisplayModal }) => { //instead of props
   /* Insert React */
   const { id, location, urls, user } = photoList;
 
   return (<div className="photo-list__item">
-
     {state.loading ? (
       <Skeleton variant="rectangular" width="300px" height="200px" />
     ) : (<>
